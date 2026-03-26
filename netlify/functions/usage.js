6533f9420d186be455a043bbf0d079576b2ae9b9
seba4444-web <seba4444@gmail.com>
2026-03-26 15:21:42 +0100

    Create usage.js

---
+++ netlify/functions/usage.js
@@ -0,0 +1,22 @@
+exports.handler = async function(event) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+  try {
+    const { apiKey } = JSON.parse(event.body);
+    const response = await fetch('https://api-free.deepl.com/v2/usage', {
+      headers: { 'Authorization': 'DeepL-Auth-Key ' + apiKey }
+    });
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
